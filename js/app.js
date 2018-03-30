e14a3e4a1e25d4c8fea39f98c8ae58266582b262
Daria Puch <dar-puch@users.noreply.github.com>
2018-03-30 13:36:39 +0200

    responsive styling

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ let cardsList = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-c
 let open = [],
   movesCounter = 0,
   countMatched = 0,
-  overlay = document.querySelector(".overlay"),
-  deck = document.querySelector('.deck'),
   countClicks = 0,
   timerId,
   stars;
+const deck = document.querySelector('.deck'),
+      overlay = document.querySelector(".overlay");
 
-cardsList = cardsList.concat(cardsList); //each card appears twice
+cardsList = cardsList.concat(cardsList); //each card appears twice so we double elements
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -27,7 +27,7 @@ function shuffle(array) {
   return array;
 }
 
-function prepareDeck() { // shuffle and assign classes
+function prepareDeck() { // shuffle and assign classes from cardList array
   shuffle(cardsList);
   let deckLi = deck.getElementsByTagName('li');
   for (let i = 0; i < deckLi.length; i++) {
@@ -51,7 +51,7 @@ function checkCard(currentCard) {
   if (open.length === 2) { //two cards to compare
     countMoves(); //one move = two cards
     let recentOpen = open[1]; //card added before current
-    if (recentOpen.children[0].className === currentCard.children[0].className) { //see if they have the same icons
+    if (recentOpen.children[0].className === currentCard.children[0].className) { //check if they have the same icons
       matchCard(currentCard, recentOpen); //mark both of them as matched
     } else {
       hideCard(currentCard, recentOpen); //if cards not match, hide both
@@ -62,16 +62,16 @@ function checkCard(currentCard) {
 function matchCard(card1, card2) {
   countMatched += 1;
   open.length = 0;
-  setTimeout(function() {
-    card1.classList.add("match", "match-effect");
+  setTimeout(function() { //wait 1 second before applying effects
+    card1.classList.add("match", "match-effect"); //loop instead?
     card2.classList.add("match", "match-effect");
     card1.classList.remove("open", "show");
     card2.classList.remove("open", "show");
     setTimeout(function() {
-      card1.classList.remove("match-effect");
+      card1.classList.remove("match-effect"); //glow for 1 sec
       card2.classList.remove("match-effect");
     }, 1000)
-    if (countMatched === 2) { //change to 16 after tests
+    if (countMatched === 8) {
       gameCompleted();
     }
   }, 1000);
@@ -81,7 +81,7 @@ function countMoves() {
   movesCounter += 1;
   let step = 10;
   const movesLabel = document.querySelector(".moves-label");
-  if (movesCounter === 1) {
+  if (movesCounter === 1) { //print Move instead of Moves when there's one
     movesLabel.textContent = "Move";
   } else {
     movesLabel.textContent = "Moves";
@@ -95,10 +95,20 @@ function countMoves() {
 function timing(start) {
   timerId = setInterval(function() {
     now = Date.now();
-    elapsedTime = Math.floor((now - start) / 1000);
+    elapsedTime = Math.floor((now - start) / 1000); //time in seconds
     document.querySelector(".time").textContent = elapsedTime;
   }, 1000);
 
+}
+
+function updateStars(number) { //generate and appends passed number of li with stars
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < number; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = '<i class="fa fa-star"></i>';
+    fragment.appendChild(li);
+  }
+  return fragment;
 }
 
 function gameCompleted() {
@@ -112,18 +122,9 @@ function gameCompleted() {
   document.querySelector(".result-time").textContent = elapsedTime;
 }
 
-function updateStars(number) {
-  let fragment = document.createDocumentFragment();
-  for (let i = 0; i < number; i++) {
-    let li = document.createElement('li');
-    li.innerHTML = '<i class="fa fa-star"></i>';
-    fragment.appendChild(li);
-  }
-  return fragment;
-}
 
 function resetAll() {
-open.length = 0; //clear array
+open.length = 0;
 movesCounter = 0;
 countMatched = 0;
 countClicks = 0;
@@ -131,7 +132,7 @@ stars = document.querySelector(".stars").childElementCount;
 document.querySelector(".moves").textContent = 0;
 clearInterval(timerId);
 document.querySelector(".time").textContent = 0;
-fragment = updateStars(3-stars);
+fragment = updateStars(3-stars); //add missing stars to get 3
 document.querySelector(".stars").appendChild(fragment) //display stars prepared above
 let children = document.querySelector(".deck").children;
 for (let i = 0; i < children.length; i++) { //clear classes
@@ -140,14 +141,16 @@ for (let i = 0; i < children.length; i++) { //clear classes
 prepareDeck();
 }
 
-prepareDeck(); //mix and display cards
+//end of functions and variables, begin of the game
+
+prepareDeck();
 
 deck.addEventListener("click", function(event) {
   let target = event.target;
   if (!(target.classList.contains("match") || target.classList.contains("show")) && target.tagName === "LI") { //exclude other clicks than on li and with class open or match
     open.unshift(target); //add clicked card to array
     countClicks += 1;
-    if (countClicks === 1) {
+    if (countClicks === 1) { //start game with first click
       start = Date.now();
       timing(start);
     }
